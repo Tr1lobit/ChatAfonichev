d5eac3be4a26e608f1167ed8bc1e18b240786cb8
wsrvs@outlook.com <wsrvs@outlook.com>
2023-05-12 15:16:04 +0300

    Добавьте файлы проекта.

---
+++ ChatAfonichev/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatAfonichev
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            HttpClient httpClient = new HttpClient();
+            InitializeComponent();
+            if(!string.IsNullOrEmpty(Properties.Settings.Default.login) && !string.IsNullOrEmpty(Properties.Settings.Default.password))
+            {
+            LoginTb.Text = Properties.Settings.Default.login;
+            PassTb.Text = Properties.Settings.Default.password;
+            }
+
+        }
+
+        private void EnterBtnClick(object sender, RoutedEventArgs e)
+        {
+            var content = new Data { username = LoginTb.Text, password = PassTb.Text };
+            HttpContent httpContent = new StringContent(JsonConvert.SerializeObject(content), Encoding.UTF8, "application/json");
+
+            //if((bool)RememberCheck.IsChecked)
+            //{
+            //    Properties.Settings.Default.login = LoginTb.Text;
+            //    Properties.Settings.Default.password = PassTb.Text;
+            //    Properties.Settings.Default.Save();
+            //}
+            //else
+            //{
+            //    Properties.Settings.Default.login = String.Empty;
+            //    Properties.Settings.Default.password = String.Empty;
+            //    Properties.Settings.Default.Save();
+            //}
+        }
+
+        private void LoginTbGotFocus(object sender, RoutedEventArgs e)
+        {
+            if(LoginTb.Text == "Введите логин")
+            LoginTb.Text = string.Empty;
+        }
+
+        private void LoginTbLostFocus(object sender, RoutedEventArgs e)
+        {
+            if(LoginTb.Text == string.Empty)
+            LoginTb.Text = "Введите логин";
+        }
+
+        private void PassTbGotFocus(object sender, RoutedEventArgs e)
+        {
+            if(PassTb.Text == "Введите пароль")
+            PassTb.Text = string.Empty;
+        }
+
+        private void PassTbLostFocus(object sender, RoutedEventArgs e)
+        {
+            if(PassTb.Text == string.Empty)
+            PassTb.Text = "Введите пароль";
+        }
+
+        public class Data
+        {
+            public string username { get; set; }
+            public string password { get; set; }
+        }
+    }
+}
